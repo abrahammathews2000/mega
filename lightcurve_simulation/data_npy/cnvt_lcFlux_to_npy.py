93ab89c4f2214f14daf4b244b9ac78b174814542
abrahammathews2000 <abrahammathews2000@gmail.com>
2023-12-08 12:30:55 +0530

    Modifying code to simulate LC for different LDCs not there in training dataset and also to test using the ml model trained on noiseless interpolate light curve

--- lightcurve_simulation/data_npy/cnvt_lcFlux_to_npy.py
+++ lightcurve_simulation/data_npy/cnvt_lcFlux_to_npy.py
@@ -67,15 +67,15 @@ class SaveLcAsNpy:
 
 
 # What is the folder path for the input raw light curves?
-# raw_lc_dir = '../../data/test/raw/lc/lc_9_shape_1/' # Test
+raw_lc_dir = '../../data/test/raw/lc/lc_17_shape_1/' # Test
 # raw_lc_dir = '../../data/vald/raw/lc/lc_9_shape_1/' # Validation
-raw_lc_dir = '../../data/train/raw/lc/lc_9_shape_5/' # Train
+# raw_lc_dir = '../../data/train/raw/lc/lc_9_shape_5/' # Train
 
 # Where do you want to save the output npy light curves file?
 # Include the full path including file name
-# output_npy_lc_path = '../../data/test/npy/lc/lc_9_shape_1.npy' # Test
+output_npy_lc_path = '../../data/test/npy/lc/lc_17_shape_1.npy' # Test
 # output_npy_lc_path = '../../data/vald/npy/lc/lc_9_shape_1.npy' # Validation
-output_npy_lc_path = '../../data/train/npy/lc/lc_9_shape_5.npy'# Train
+# output_npy_lc_path = '../../data/train/npy/lc/lc_9_shape_5.npy'# Train
 
 print('raw_lc_dir = ',raw_lc_dir)
 print('output_npy_lc_path = ',output_npy_lc_path)
