4b2d1aa55807403d16cf2c9ff5d6fdff22cafda4
abrahammathews2000 <abrahammathews2000@gmail.com>
2024-03-26 16:31:48 +0530

    Varying different input links before and after simulation and edit of log files

--- lightcurve_simulation/data_raw/frontend_rand_param_lc_gen.py
+++ lightcurve_simulation/data_raw/frontend_rand_param_lc_gen.py
@@ -11,7 +11,7 @@ import time
 
 # Where do you want to save the lcs?
 # !! Change this everytime you run !!
-save_lc_in_folder = '/scratch/abraham/Documents/mega_git/mega/data/vald/raw/lc/lc_10_shape_multisize_multiLDC/'
+save_lc_in_folder = ' '
 # save_lc_in_folder = '../../data/test/raw/lc/lc_17_shape_1/' # For Test
 # save_lc_in_folder = '../../data/vald/raw/lc/lc_9_shape_1/' # For Validation
 #
@@ -20,15 +20,16 @@ save_lc_in_folder = '/scratch/abraham/Documents/mega_git/mega/data/vald/raw/lc/l
 # Give the npy file path for the shape
 # shape_dir = 
 # shape_dir = '../../data/test/npy/shape/shape_1.npy' # For Test
-shape_dir = '/scratch/abraham/Documents/mega_git/mega/data/vald/npy/shape/shape_1.npy' # For Validation
+shape_dir = '/scratch/abraham/Documents/mega_git/mega/data/test/npy/shape/shape_1.npy' # For Validation
 
 # if not os.path.exists(save_lc_in_folder):
 #     os.mkdir(save_lc_in_folder)
 
+print("Which shapes do you need to simulate to get the lcs? \n ",shape_dir)
+print("Where do you want to save the lcs? \n ",save_lc_in_folder)
 
-
-
-
+print("LD_Coeff_dist_start = [0.1,0.0], LD_Coeff_dist_stop = [0.9,0.2],","\n",
+                    "star2mega_radius_ratio_start = 3, star2mega_radius_ratio_stop = 20")
 
 user_input = input("Do you want to run the code? (y/n): ")
 tic = time.perf_counter()
