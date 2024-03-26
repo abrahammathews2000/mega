ffcea66d5df57a83fbe3b6bb4dfdefd7b77d507c
abrahammathews2000 <abrahammathews2000@gmail.com>
2024-03-26 16:30:08 +0530

    Added rest the code for ML training of unf diff of parameters (training not done yet)

--- ml_training/ml_train_lightcurve_unf_dist_LDC_size.py
+++ ml_training/ml_train_lightcurve_unf_dist_LDC_size.py
@@ -43,3 +43,174 @@ if len(vald_lc)==len(vald_shape):
     print("Vald Set: No. of LC = No. of shapes")
 else:
     sys.exit("Vald Set: No. of LC = No. of shapes")
+
+# 2. Normalize the image, convert to opacity map
+## Train Set
+train_shape = train_shape/np.amax(train_shape)
+train_shape_where_0 = np.where(train_shape == 0)
+train_shape_where_1 = np.where(train_shape == 1)
+train_shape[train_shape_where_0] = 1  # 1 represent the shape (1 opacity)
+train_shape[train_shape_where_1] = 0  # 0 represent background (0 opacity)
+
+## Valdn Set
+vald_shape = vald_shape/np.amax(vald_shape)
+vald_shape_where_0 = np.where(vald_shape == 0)
+vald_shape_where_1 = np.where(vald_shape == 1)
+vald_shape[vald_shape_where_0] = 1  # 1 represent the shape (1 opacity)
+vald_shape[vald_shape_where_1] = 0  # 0 represent background (0 opacity)
+print("Normalized the shape")
+
+# 3. Normalize the lightcurves
+## - Train Set
+train_lc_scaled = np.zeros(train_lc.shape)
+for i in np.arange(len(train_lc_scaled)):
+    train_lc_scaled[i] = (train_lc[i] - np.amin(train_lc[i]))/(np.amax(train_lc[i]) - np.amin(train_lc[i]))
+
+## - Vald Set
+vald_lc_scaled = np.zeros(vald_lc.shape)
+for i in np.arange(len(vald_lc_scaled)):
+    vald_lc_scaled[i] = (vald_lc[i] - np.amin(vald_lc[i]))/(np.amax(vald_lc[i]) - np.amin(vald_lc[i]))
+print("Normalized the light curves")
+
+
+# Add flat line towards left and right of dip
+# 10 data points on each side
+# 4. Extend the lightcurves
+## - Train Set
+train_lc_scaled_append = np.ones((train_lc.shape[0],120))
+print('train_lc_scaled_append.shape = ',train_lc_scaled_append.shape)
+print("len(train_lc_scaled_append[0,10:110]) = ",len(train_lc_scaled_append[0,10:110]))
+
+for i in np.arange(len(train_lc_scaled)):
+    train_lc_scaled_append[i,10:110] = train_lc_scaled[i]
+
+## - Vald Set
+vald_lc_scaled_append = np.ones((vald_lc.shape[0],120))
+for i in np.arange(len(vald_lc_scaled)):
+    vald_lc_scaled_append[i,10:110] = vald_lc_scaled[i]
+print("Extended the light curves")
+
+
+# Verification
+# Plot - Train LCs
+plt.clf()
+num = 3
+fig,ax=plt.subplots(num,2, figsize=(4,3), gridspec_kw={ 'width_ratios': [2,1],
+        'wspace': 0.2,'hspace': 0.4})
+
+ax[0][1].set_title('Shape',size=15)
+ax[0][0].set_title('Light Curve (Train Dataset)',size=15)
+ax[num-1][0].set_xlabel('Phase',size=13)
+ph = np.linspace(-1,1,len(train_lc_scaled_append[0]))
+# advance = 60
+
+i = 0
+for i in np.arange(0,num):
+    k = np.random.randint(0, len(train_lc_scaled_append)-1)
+    ax[i][1].tick_params(left = False, right = False , labelleft = False ,labelbottom = False, bottom = False)
+    if(i<num-1): ax[i][0].tick_params(labelbottom = False, bottom = False)
+    img = ax[i][1].imshow(train_shape[k],cmap='inferno')
+    plt.colorbar(img)
+    ax[i][0].set_ylabel('Flux',size=13)
+    ax[i][0].set_ylim(-0.5,1.5)
+#     ax[i][0].scatter(ph, vald_lc_scaled_append[k],color = 'black',marker='.')
+    ax[i][0].plot(ph, train_lc_scaled_append[k],color = 'tab:red',linewidth='2')
+    ax[i][0].grid('on')
+    i = i + 1
+plt.savefig('/scratch/abraham/Documents/mega_git/mega/ml_model/mar26_2024_model/plot_train_lc.png')
+plt.close()
+
+# ML Pipeline
+## Train Set
+train_dataset = tf.data.Dataset.from_tensor_slices((train_lc_scaled_append,train_shape))
+train_dataset = train_dataset.cache()
+train_dataset = train_dataset.shuffle(len(train_dataset))
+train_dataset = train_dataset.batch(100)
+train_dataset = train_dataset.prefetch(tf.data.AUTOTUNE)
+print(train_dataset)
+
+## Vald Set
+vald_dataset = tf.data.Dataset.from_tensor_slices((vald_lc_scaled_append,vald_shape))
+vald_dataset = vald_dataset.batch(100)
+vald_dataset = vald_dataset.cache()
+vald_dataset = vald_dataset.prefetch(tf.data.AUTOTUNE)
+print(vald_dataset)
+
+# CNN Model
+input_shape = np.array(np.shape(train_lc_scaled_append[0]))
+print("np.shape(input_shape) = ",input_shape[0])
+
+output_shape = np.array(np.shape(train_shape[0]))
+print("np.shape(input_shape) = ",output_shape[0],output_shape[1])
+
+START = input_shape[0] # 94 #
+END = output_shape[0] # 38  # pixel size along one axis
+print("Start = ",START)
+print("End = ",END)
+
+conv_ip = keras.layers.Input(shape=(START,),name='Input')
+x= keras.layers.Reshape((START, 1), input_shape=(START,),name='reshape_1')(conv_ip)
+x= keras.layers.BatchNormalization()(x)
+
+x=keras.layers.Conv1D(16,kernel_size=5,strides=1,activation='relu',name='conv16_5', padding='same')(x)
+x=keras.layers.Conv1D(16,kernel_size=5,strides=1,activation='relu',name='second_conv16_5', padding='same')(x)
+x=keras.layers.MaxPool1D(5,strides=2,data_format='channels_last',name='maxpool_1', padding='same')(x) #400
+
+x=keras.layers.Conv1D(32,kernel_size=5,strides=1,activation='relu',name='first_conv32_5', padding='same')(x)
+x=keras.layers.Conv1D(32,kernel_size=5,strides=1,activation='relu',name='second_conv32_5', padding='same')(x)
+x=keras.layers.MaxPool1D(5,strides=2,data_format='channels_last',name='maxpool_2', padding='same')(x) #200
+
+x=keras.layers.Conv1D(64,kernel_size=5,strides=1,activation='relu',name='first_conv64_5', padding='same')(x)
+x=keras.layers.Conv1D(64,kernel_size=5,strides=1,activation='relu',name='second_conv64_5', padding='same')(x)
+x=keras.layers.MaxPool1D(5,strides=2,data_format='channels_last',name='maxpool_3', padding='same')(x) #100
+
+x=keras.layers.Flatten(name='flat_1')(x)
+
+x2=keras.layers.Dense(256,name='dense_layer_5',activation='relu')(x)
+x2=keras.layers.Dense(256,name='dense_layer_6',activation='relu')(x2)
+
+x2= keras.layers.Dense(END**2,name='dense_layer_u',activation='relu')(x2)
+x2 = keras.layers.Reshape(target_shape=(END, END, 1),name='reshape_2')(x2)
+
+x2=keras.layers.Conv2D(32,kernel_size=(3,3),strides=1,activation='relu',name='second_conv64_52', padding='same')(x2)
+x2=keras.layers.Conv2D(32,kernel_size=(3,3),strides=1,activation='relu',name='second_conv64_522', padding='same')(x2)
+x2=keras.layers.Conv2D(16,kernel_size=(3,3),strides=1,activation='relu',name='second_conv64_524', padding='same')(x2)
+x2=keras.layers.Conv2D(1,kernel_size=3,strides=1,activation='relu',name='second_conv64_53', padding='same')(x2)
+conv_op = keras.layers.Reshape(target_shape=(END, END),name='reshape_3')(x2)
+model = keras.Model(inputs=conv_ip, outputs=conv_op, name="predict_shape_from_LC")
+model.summary()
+print("Model is defined")
+
+# Compile the model
+model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.0001), loss='mse')
+print("Model is compiled")
+
+#Patience early stopping
+es = keras.callbacks.EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=30)
+print("Early stopping defined")
+
+#Learning rate scheduler
+def step_decay(epoch):
+	initial_lrate = 0.001
+	drop = 0.5
+	epochs_drop = 20
+	lrate = initial_lrate * math.pow(drop, math.floor((1+epoch)/epochs_drop))
+	return lrate
+lr_sched = keras.callbacks.LearningRateScheduler(step_decay)
+print("Learning rate scheduler defined")
+
+# Training 
+print("Training will start now")
+# history = model.fit(train_lc_scaled_interpol, train_shape, epochs=200,verbose=2, validation_data=(vald_lc_scaled_interpol,vald_shape),callbacks=[es,lr_sched])
+history = model.fit(train_dataset, epochs=200,verbose=2, validation_data=vald_dataset,callbacks=[es,lr_sched])
+
+# Save Model (Uncomment onlt if you are really running this)
+save_model(model, "/scratch/abraham/Documents/mega_git/mega/ml_model/mar26_2024_model/mar26_2024_model_unfDist_LDC_size.h5")
+print("Model saved")
+
+
+
+
+
+tf.keras.backend.clear_session()
+print("End of code")
