219a926a9b94d7a2463f5dd408c6da03a70eb7b8
abrahammathews2000 <abrahammathews2000@gmail.com>
2024-04-20 15:50:54 +0530

    Modifying slightly 2 codes to generate shapes

--- lightcurve_simulation/data_raw/create_a_shape.py
+++ lightcurve_simulation/data_raw/create_a_shape.py
@@ -8,7 +8,17 @@ from bezier import get_random_points,get_bezier_curve
 import cv2
 
 class ShapeGen:
-    def __init__(self, rad, edgy,noEdges,name,Rmega_star,shape_pixel,index_shapefolder):
+    def __init__(
+            self, 
+            rad, 
+            edgy, 
+            noEdges, 
+            name, 
+            Rmega_star, 
+            shape_pixel, 
+            img_save_folder_path,
+        ):
+
         self.rad = rad
         self.edgy = edgy
         self.noEdges = noEdges
@@ -16,67 +26,51 @@ class ShapeGen:
         self.Rmega_star = Rmega_star
         self.shape_pixel = shape_pixel
         self.coord_bezier = self.create_bezier_shape()
-        self.index_shapefolder = index_shapefolder
+        self.img_save_folder_path = img_save_folder_path
 
     def create_bezier_shape(self):
         # Bezier shape generation
         a = get_random_points(n=self.noEdges, scale=1)
         x, y, _ = get_bezier_curve(a, rad=self.rad, edgy=self.edgy)
-        x = (x - np.mean(x)) * self.Rmega_star * 2
-        y = (y - np.mean(y)) * self.Rmega_star * 2
-        z = np.zeros(len(x))
-        # print(" Inside create_bezier_shape")
-        # print("x = ",x)
-        # print("y = ",y)
-        temp = np.stack((x, y, z), axis=1)
-        # print("temp = ",temp)
-        return np.stack((x, y, z), axis=1)
 
-    # def save_coord(self):
-    #     np.savetxt("./generatedData/shape_coord/coord0" + str(self.name) + '.csv', self.coord_bezier, delimiter=',')
-    #     # print("shape_coord_" + str(self.name) + "_saved")
+        x = (x-np.mean(x)) * self.Rmega_star * 2
+        y = (y-np.mean(y)) * self.Rmega_star * 2
+        z = np.zeros(len(x))
+
+        return np.stack((x, y, z), axis=1)
 
     def save_png_filled(self):
         plt.clf()
         plt.figure(figsize=(self.shape_pixel, self.shape_pixel))
-        plt.tick_params(left=False, right=False, labelleft=False, labelbottom=False, bottom=False)
-        x = self.coord_bezier[:,0]
-        y = self.coord_bezier[:,1]
+        plt.tick_params(
+            left=False, 
+            right=False, 
+            labelleft=False, 
+            labelbottom=False, 
+            bottom=False
+        )
 
-        # print("Inside save_png_filled")
-        # print("x = ",x)
-        # print("y = ",y)
+        x = self.coord_bezier[:, 0]
+        y = self.coord_bezier[:, 1]
+
         plt.plot(x, y, "black")
-        plt.fill(x, y,"black")
-        # plt.margins(x=0)
-        # plt.margins(y=0)
+        plt.fill(x, y, "black")
         plt.axis('off')  # To remove frame box
-        plt.savefig('/home/abraham/Documents/ms_proj_shape_lc_gen/data_raw/shape/'+str(self.index_shapefolder)+'/shape0' + str(self.name) + '.png',bbox_inches="tight",pad_inches=0)
-        # plt.savefig("./generatedData/shape_filled/shape0" + str(self.name) + ".png",bbox_inches="tight",pad_inches=0)
+        plt.savefig(
+            self.img_save_folder_path+'/shape0'+str(self.name)+'.png',
+            bbox_inches="tight",
+            pad_inches=0
+        )
         plt.close()
-        # Convert RGB to grayscale
-        image2cnvt = cv2.imread('/home/abraham/Documents/ms_proj_shape_lc_gen/data_raw/shape/'+str(self.index_shapefolder)+'/shape0' + str(self.name) + '.png')
-        gray_cnvtd = cv2.cvtColor(image2cnvt, cv2.COLOR_BGR2GRAY)
-        cv2.imwrite('/home/abraham/Documents/ms_proj_shape_lc_gen/data_raw/shape/'+str(self.index_shapefolder)+'/shape0' + str(self.name) + '.png', gray_cnvtd)
-        # print("shape_filled_" + str(self.name) + "_saved")
 
-    # def save_png_unfilled(self):
-    #     plt.clf()
-    #     plt.figure(figsize=(self.shape_pixel, self.shape_pixel))
-    #     plt.tick_params(left=False, right=False, labelleft=False, labelbottom=False, bottom=False)
-    #     x = self.coord_bezier[:,0]
-    #     y = self.coord_bezier[:,1]
-    #     plt.plot(x, y, "black")
-    #     # plt.margins(x=0)
-    #     # plt.margins(y=0)
-    #     plt.axis('off')  # To remove frame box
-    #     plt.savefig("./generatedData/shape_unfilled/shape0" + str(self.name) + ".png",bbox_inches="tight",pad_inches=0)
-    #     plt.close()
-    #     # Convert RGB to grayscale
-    #     image2cnvt = cv2.imread("./generatedData/shape_unfilled/shape0" + str(self.name) + ".png")
-    #     gray_cnvtd = cv2.cvtColor(image2cnvt, cv2.COLOR_BGR2GRAY)
-    #     cv2.imwrite("./generatedData/shape_unfilled/shape0" + str(self.name) + ".png", gray_cnvtd)
-    #     # print("shape_unfilled_" + str(self.name) + "_saved")
+        # Convert RGB to grayscale
+        image2cnvt = cv2.imread(self.img_save_folder_path+'/shape0'+str(self.name)+'.png')
+        gray_cnvtd = cv2.cvtColor(image2cnvt, cv2.COLOR_BGR2GRAY)
+        cv2.imwrite(
+            self.img_save_folder_path+'/shape0'+str(self.name)+'.png', 
+            gray_cnvtd
+        )
 
     def __del__(self):
         print('Destructor called, shape deleted.')
+        
--- lightcurve_simulation/data_raw/create_shapes.py
+++ lightcurve_simulation/data_raw/create_shapes.py
@@ -6,42 +6,40 @@ import time
 
 tic = time.perf_counter()
 
-#Change everytime you run#
-index_shapefolder = 8
+img_save_folder_path = '/scratch/abraham/Documents/mega_git/mega/data/train/raw/shape/20_April_2024/'
 
-if not os.path.exists('/home/abraham/Documents/ms_proj_shape_lc_gen/data_raw/shape/'):
-    os.mkdir('/home/abraham/Documents/ms_proj_shape_lc_gen/data_raw/shape/')
-# if not os.path.exists('./generatedData/shape_coord/'):
-#     os.mkdir('./generatedData/shape_coord/')
-if not os.path.exists('/home/abraham/Documents/ms_proj_shape_lc_gen/data_raw/shape/'+str(index_shapefolder)+'/'):
-    os.mkdir('/home/abraham/Documents/ms_proj_shape_lc_gen/data_raw/shape/'+str(index_shapefolder)+'/')
-# if not os.path.exists('./generatedData/shape_unfilled/'):
-#     os.mkdir('./generatedData/shape_unfilled/')
+if not os.path.exists(img_save_folder_path):
+    os.mkdir(img_save_folder_path)
 
-# rad_array_random = np.linspace(0, 0.9, 5) #np.unique(np.random.uniform(0,1,30))
-rad_array =  [0.12, 0.23, 0.45, 0.73, 0.91] # np.linspace(0.1, 0.9, 5) # np.array([1])    # rad_array =
-                                # Do to limited RAM size only one element is give
-                                # rad_array for every run
-                                
-# edgy_array_random = np.linspace(0, 9, 10) # np.unique(np.random.randint(0,20,30))   
-edgy_array =  [1.2, 3.3, 5.4, 7.8, 9.2] # np.linspace(1, 9, 5)   # np.linspace(0, 9, 10)
+rad_array =  [0.1, 0.2, 0.3, 0.4, 0.5]                                
+edgy_array =  [1.0, 2.0, 3.0, 4.0, 5.0] 
+edges_array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
 print('rad_array =', rad_array)
-print('edgy_array = ',edgy_array)
-edges_array = [3,4,5,6,7,8,9,10,11,12]
+print('edgy_array =',edgy_array)
+print('edges_array =',edges_array)
 variety = [0]
-name = 0
 
-for var_el in variety:
-    for rad_el in rad_array:
-        for edgy_el in edgy_array:
-            for edges_el in edges_array:
-                t = ShapeGen(rad=rad_el, edgy=edgy_el,noEdges=edges_el,name=name,Rmega_star=0.25,shape_pixel=0.50,index_shapefolder=index_shapefolder)
-                # t.save_coord()
-                t.save_png_filled()
-                # t.save_png_unfilled()
-                del t
-                print("Shape no. ",name)
-                name = name + 1
+name = 0
+for rad_el in rad_array:
+    for edgy_el in edgy_array:
+        for edges_el in edges_array:
+            t = ShapeGen(
+                rad=rad_el,
+                edgy=edgy_el,
+                noEdges=edges_el,
+                name=name,
+                Rmega_star=0.25,
+                shape_pixel=0.50,
+                img_save_folder_path=img_save_folder_path
+            )
+
+            t.save_png_filled()
+            del t
+
+            print("Shape no: ",name)
+            name = name + 1
+
 print("Shape generation done")
 
 toc = time.perf_counter()
