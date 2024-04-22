0f608bf0b014ef4fb154f0c15fdf25984aba2c30
abrahammathews2000 <abrahammathews2000@gmail.com>
2024-04-22 16:54:54 +0530

    code to simulate one more round of randomly distributed LDC and size. LD_Coeff_dist_start=[0, -0.15], LD_Coeff_dist_stop=[1, 0.55]. star2mega_radius_ratio_start=2, star2mega_radius_ratio_stop=350. commiting before running.

--- lightcurve_simulation/data_raw/backend_rand_param_batch_lc_gen.py
+++ lightcurve_simulation/data_raw/backend_rand_param_batch_lc_gen.py
@@ -16,8 +16,8 @@ import concurrent.futures
 
 class LcGenerator:
     def __init__(self, shape_dir, save_lc_folder_name, 
-                 LD_Coeff_dist_start = [0.1,0.05], LD_Coeff_dist_stop = [0.9,0.2],
-                 star2mega_radius_ratio_start = 3,star2mega_radius_ratio_stop = 20):
+                 LD_Coeff_dist_start=[0, -0.15], LD_Coeff_dist_stop=[1, 0.55],
+                 star2mega_radius_ratio_start=2, star2mega_radius_ratio_stop=350):
         rng = np.random.default_rng()
         self.save_lc_folder_name = save_lc_folder_name
         self.shape_dir = shape_dir  
@@ -32,38 +32,42 @@ class LcGenerator:
         print(self.batch_names[start_index:stop_index])
 
         for batch in self.batch_names[start_index:stop_index]:
-            batch_link = self.shape_dir + batch
-            print('batch_link = ',batch_link)
+            batch_link = self.shape_dir+batch
+            print('batch_link =', batch_link)
             self.y = np.load(batch_link)
-            self.y = self.y / np.amax(self.y) # Normalizing images pixel value = [0,1]
-            print('self.y = ',self.y)
+            self.y = self.y/np.amax(self.y) # Normalizing images pixel value = [0,1]
+            print('self.y =', self.y)
             self.y_shape = np.array(np.shape(self.y[0]))
-            self.radius_mega = self.y_shape[0] / 2
+            self.radius_mega = self.y_shape[0]/2
 
-            where_0 = np.where(self.y == 0)  
-            where_1 = np.where(self.y == 1)
+            where_0 = np.where(self.y==0)  
+            where_1 = np.where(self.y==1)
             self.y[where_0] = 1  # opacity map. 0 = no opacity (transparent); 1 = fully opaque
             self.y[where_1] = 0
 
             # Make the relative radius as uniform distribution
             # print(len(self.y))
             star2mega_radius_ratio = rng.integers(low = star2mega_radius_ratio_start, high=star2mega_radius_ratio_stop, size=len(self.y))
-            np.savetxt("/scratch/abraham/Documents/mega_git/mega/data/train/info/lc_10_shape_multisize_multiLDC_details/star2mega_radius_ratio_details/star2megaRadius_"+str(batch)+".csv", 
-                       star2mega_radius_ratio, delimiter=',')
-            print("star2mega_radius_ratio details save in: ")
-            print("/scratch/abraham/Documents/mega_git/mega/data/train/info/lc_10_shape_multisize_multiLDC_details/star2mega_radius_ratio_details/star2megaRadius_"+str(batch)+".csv")
+            # np.savetxt("/scratch/abraham/Documents/mega_git/mega/data/train/info/lc_10_shape_multisize_multiLDC_details/star2mega_radius_ratio_details/star2megaRadius_"+str(batch)+".csv", 
+                    #    star2mega_radius_ratio, delimiter=',')
+            # print("star2mega_radius_ratio details save in: ")
+            # print("/scratch/abraham/Documents/mega_git/mega/data/train/info/lc_10_shape_multisize_multiLDC_details/star2mega_radius_ratio_details/star2megaRadius_"+str(batch)+".csv")
     
             # Make the LDCs  as uniform distribution
-            a = np.random.default_rng().uniform(low = LD_Coeff_dist_start[0],high = LD_Coeff_dist_stop[0],size = len(self.y)) # np.ones(5)*0.3 # 
-            b = np.random.default_rng().uniform(low = LD_Coeff_dist_start[1],high = LD_Coeff_dist_stop[1],size = len(self.y)) # np.ones(5)*0.6 #
+            a = np.random.default_rng().uniform(low=LD_Coeff_dist_start[0], 
+                                                high=LD_Coeff_dist_stop[0],
+                                                size=len(self.y)) 
+            b = np.random.default_rng().uniform(low=LD_Coeff_dist_start[1], 
+                                                high=LD_Coeff_dist_stop[1],
+                                                size=len(self.y))
             LD_Coeff = np.array([a,b]).T
-            np.savetxt("/scratch/abraham/Documents/mega_git/mega/data/train/info/lc_10_shape_multisize_multiLDC_details/LDC_details/LDCs_"+str(batch)+".csv", 
-                       LD_Coeff, delimiter=',')
-            print("LDC details save in: ")
-            print("/scratch/abraham/Documents/mega_git/mega/data/train/info/lc_10_shape_multisize_multiLDC_details/LDC_details/LDCs_"+str(batch)+".csv")
+            # np.savetxt("/scratch/abraham/Documents/mega_git/mega/data/train/info/lc_10_shape_multisize_multiLDC_details/LDC_details/LDCs_"+str(batch)+".csv", 
+                    #    LD_Coeff, delimiter=',')
+            # print("LDC details save in: ")
+            # print("/scratch/abraham/Documents/mega_git/mega/data/train/info/lc_10_shape_multisize_multiLDC_details/LDC_details/LDCs_"+str(batch)+".csv")
     
             with concurrent.futures.ProcessPoolExecutor(max_workers=20) as executor:
-                results = [executor.submit(self.gen_lc,temp_shape=self.y[i],name=str(batch)+'_'+str(i),LD_Coeff=LD_Coeff[i],star2mega_radius_ratio=star2mega_radius_ratio[i]) for i in np.arange(len(self.y))]
+                results = [executor.submit(self.gen_lc, temp_shape=self.y[i], name=str(batch)+'_'+str(i), LD_Coeff=LD_Coeff[i], star2mega_radius_ratio=star2mega_radius_ratio[i]) for i in np.arange(len(self.y))]
 
                 num_workers = executor._max_workers
                 print("Number of process pool workers:", num_workers)
@@ -72,7 +76,7 @@ class LcGenerator:
                     print(f.result())
                 
             #---
-    def gen_lc(self, temp_shape,name,LD_Coeff,star2mega_radius_ratio):
+    def gen_lc(self, temp_shape, name, LD_Coeff, star2mega_radius_ratio):
         radius_star = star2mega_radius_ratio * self.radius_mega
         pad_width_for_mega = int(radius_star - self.radius_mega)
         
@@ -80,85 +84,20 @@ class LcGenerator:
 
         times = np.linspace(-35, 35, 1000)
 
-        annulus = TransitingImage(opacitymat=z, v=0.4, t_ref=0., t_arr=times,LDlaw='quadratic',LDCs=LD_Coeff)
+        annulus = TransitingImage(opacitymat=z, 
+                                  v=0.4, 
+                                  t_ref=0., 
+                                  t_arr=times,
+                                  LDlaw='quadratic',
+                                  LDCs=LD_Coeff)
 
-        annulus_LC, overlapTimes = annulus.gen_LC(t_arr=times) #,gpu=True)
+        annulus_LC, overlapTimes = annulus.gen_LC(t_arr=times)
 
-        self.save_lc(annulus_LC, overlapTimes, name)
+        self.save_lc(annulus_LC, name)
 
-    def save_lc(self,lc,time,name):        
-        np.savetxt(str(self.save_lc_folder_name) +'lc_'+str(name) + ".csv", lc, delimiter=',')
-        print("lc_"+ str(name)+"saved")
+    def save_lc(self, lc, name):        
+        np.savetxt(str(self.save_lc_folder_name)+'lc_'+str(name) +".csv", lc, delimiter=',')
+        print("lc_"+str(name)+"saved")
 
     def __del__(self):
         print('Destructor called, lc deleted.')
-
-
-
-
-
-
-# ---- Old ---- #
-
-
-
-        
-        # plt.close()
-
-        # times = np.linspace(-35., 35., 500)
-        #
-        # annulus = TransitingImage(opacitymat=z, v=0.4, t_ref=0., t_arr=times)
-        # annulus.plot_grid()
-        #
-        # annulus_LC, overlapTimes = annulus.gen_LC(t_arr=times)
-        #
-        # fig, ax = plt.subplots(1, 1, figsize=(8, 6))
-        # ax.plot(overlapTimes, annulus_LC, color="#1969ea", ls="-", lw=1)
-        # ax.set_ylim(np.amin(annulus_LC) - 0.01, 1.01)
-        # plt.xlabel("Time [days]", fontsize=14)
-        # plt.ylabel("Relative flux", fontsize=14)
-        # # plt.title(r"The annulus' light curve as it transits left-to-right across the star at $v = 0.4 d^{-1}$",fontsize=16)
-        # plt.show()
-
-
-# Old Codes
-# #         # Testing on GPU
-    # #         # for i in np.arange(len(self.y)):
-    # #         #     self.gen_lc(temp_shape=self.y[i],name=i)
-    # #         # 
-
-    # #         #---
-    # #         # Your large numpy array with 100,000 2D matrices
-    # #         # large_array = ...  # Your data here
-
-    # #         # Define a batch size
-    # #         # self.batch_size = 10  # You can adjust this based on your system's capabilities
-
-    # #         # Calculate the total number of batches
-    # #         # self.total_batches = len(self.y) // self.batch_size + 1
-            
-    # #         print("len(self.y) = ",len(self.y))
-    # #         print("self.batch_size = ",self.batch_size)
-    # #         print("total batches = ",self.total_batches)
-    # #         print('range(self.total_batches) =',range(self.total_batches))
-    # #         # Process the data in batches
-    #         for j in range(self.total_batches):
-    #             print(" batch = ",j)
-    #             start_idx = j * self.batch_size
-    #             end_idx = (j + 1) * self.batch_size
-    #             if j == range(self.total_batches)[-1]:
-    #                 end_idx =  len(self.y) - 1
-    #             print(' start_idx = ',start_idx)
-    #             print(' end_idx = ',end_idx)
-    #             # Extract a batch of data
-    #             # self.batch = self.y[start_idx:end_idx]
-    #            # Process the batch
-            # For CPU only work with original 8bittransit
-            # with concurrent.futures.ProcessPoolExecutor() as executor:
-            #     results = [executor.submit(self.gen_lc,self.batch[i],'b'+str(j)+'_'+'s'+str(i)) for i in np.arange(len(self.batch))]
-
-            #     num_workers = executor._max_workers
-            #     print("Number of process pool workers:", num_workers)
-                
-            #     for f in concurrent.futures.as_completed(results):
-            #         print(f.result())
--- lightcurve_simulation/data_raw/frontend_rand_param_batch_lc_gen.py
+++ lightcurve_simulation/data_raw/frontend_rand_param_batch_lc_gen.py
@@ -15,29 +15,25 @@ shape_dir = '/scratch/abraham/Documents/mega_git/mega/data/train/npy/shape/shape
 
 # Where do you want to save the lcs?
 # !! Change this everytime you run !!
-save_lc_in_folder = '/scratch/abraham/Documents/mega_git/mega/data/train/raw/lc/lc_10_shape_multisize_multiLDC/' # For Train
+save_lc_in_folder = '/scratch/abraham/Documents/mega_git/mega/data/train/raw/lc/lc_11_shape_5_multisize_multiLDC/' # For Train
 
 print("Which shapes do you need to simulate to get the lcs? \n ",shape_dir)
 print("Where do you want to save the lcs? \n ",save_lc_in_folder)
 
-print("LD_Coeff_dist_start = [0.1,0.0], LD_Coeff_dist_stop = [0.9,0.2],","\n",
-                    "star2mega_radius_ratio_start = 3, star2mega_radius_ratio_stop = 20")
+print("LcGenerator(shape_dir=shape_dir, save_lc_folder_name=save_lc_in_folder,", "\n",
+                    "LD_Coeff_dist_start=[0, -0.15], LD_Coeff_dist_stop=[1, 0.55],", "\n",
+                    "star2mega_radius_ratio_start=2, star2mega_radius_ratio_stop=350)")
 user_input = input("Do you want to run the code? (y/n): ")
 tic = time.perf_counter()
 if user_input.lower() == "y":
     print("Running the program...")
     os.makedirs(save_lc_in_folder, exist_ok=True)
-    lc = LcGenerator(shape_dir = shape_dir, save_lc_folder_name = save_lc_in_folder,
-                    LD_Coeff_dist_start = [0.1,0.0], LD_Coeff_dist_stop = [0.9,0.2],
-                    star2mega_radius_ratio_start = 3, star2mega_radius_ratio_stop = 20)
+    lc = LcGenerator(shape_dir=shape_dir, save_lc_folder_name=save_lc_in_folder,
+                    LD_Coeff_dist_start=[0, -0.15], LD_Coeff_dist_stop=[1, 0.55],
+                    star2mega_radius_ratio_start=2, star2mega_radius_ratio_stop=350)
     del lc
 else:
     print("Exiting the program.")
 
 toc = time.perf_counter()
 print(toc-tic, " s")
-
-
-# # Print the size of one batch for testing
-# y = np.load(shape_dir+'shape_filled5_b0.npy')
-# print(len(y))
