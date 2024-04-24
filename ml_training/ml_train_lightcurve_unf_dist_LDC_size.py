c3309eac700b68c7fba80670705b09e936abd4b4
abrahammathews2000 <abrahammathews2000@gmail.com>
2024-04-24 14:58:41 +0530

    Cleaned by model training code + edited the input and output links, commiting before test run

--- ml_training/ml_train_lightcurve_unf_dist_LDC_size.py
+++ ml_training/ml_train_lightcurve_unf_dist_LDC_size.py
@@ -25,10 +25,16 @@ import sys
 from numpy import array,append,arange,zeros,exp,sin,random,std
 from scipy.interpolate import interp1d
 
+import time
+
+## Edit the base folder to save model, weights and plots
+base_model_folder = '/scratch/abraham/Documents/mega_git/mega/ml_model/april24_2024_model/'
+
+
 # 1. Load Dataset
 ## Load Train Set
-train_shape_dir = '/scratch/abraham/Documents/mega_git/mega/data/train/npy/shape/shape_5.npy'
-train_lc_dir = '/scratch/abraham/Documents/mega_git/mega/data/train/npy/lc/lc_10_shape_multisize_multiLDC.npy'
+train_shape_dir = '/scratch/abraham/Documents/mega_git/mega/data/train/npy/shape/concatenated_shape/train_shape_5_2times.npy'
+train_lc_dir = '/scratch/abraham/Documents/mega_git/mega/data/train/npy/lc/concatenated_lc/concat_lc_10_12_shape_5_multisize_multiLDC.npy'
 
 print(f"train_shape_dir = {train_shape_dir}")
 print(f"train_lc_dir = {train_lc_dir}")
@@ -171,11 +177,37 @@ vald_lc_horiz_scaled = scale_horizontally(vald_lc_scaled_append)
 del train_lc_scaled_append
 del vald_lc_scaled_append
 
-processed_train_lc = train_lc_horiz_scaled
-processed_vald_lc = vald_lc_horiz_scaled
+
+
+## START - Add noise
+random_generator = np.random.default_rng()
+
+SNR_array_train = random_generator.uniform(50, 500, len(train_lc_horiz_scaled))
+std_dev_train = 1/SNR_array_train
+del SNR_array_train
+
+SNR_array_vald = random_generator.uniform(50, 500, len(vald_lc_horiz_scaled))
+std_dev_vald = 1/SNR_array_vald
+del SNR_array_vald
+
+train_lc_horiz_scaled_noise = np.zeros(train_lc_horiz_scaled.shape)
+vald_lc_horiz_scaled_noise = np.zeros(vald_lc_horiz_scaled.shape)
+
+for i in np.arange(len(train_lc_horiz_scaled)):
+    train_lc_horiz_scaled_noise[i] = train_lc_horiz_scaled[i] + np.random.normal(loc=0.0, scale=std_dev_train[i], size=len(train_lc_horiz_scaled[i]))
+
+for i in np.arange(len(vald_lc_horiz_scaled)):
+    vald_lc_horiz_scaled_noise[i] = vald_lc_horiz_scaled[i] + np.random.normal(loc=0.0, scale=std_dev_vald[i], size=len(vald_lc_horiz_scaled[i]))
+## END - Add noise
+
+processed_train_lc = train_lc_horiz_scaled_noise
+processed_vald_lc = vald_lc_horiz_scaled_noise
 
 del train_lc_horiz_scaled
 del vald_lc_horiz_scaled
+
+del train_lc_horiz_scaled_noise
+del vald_lc_horiz_scaled_noise
 
 print(f"processed_train_lc = {processed_train_lc}")
 print(f"processed_vald_lc = {processed_vald_lc}")
@@ -184,29 +216,30 @@ print(f"processed_vald_lc = {processed_vald_lc}")
 # Plot - Train LCs
 plt.clf()
 num = 3
-fig,ax=plt.subplots(num,2, figsize=(4,3), gridspec_kw={ 'width_ratios': [2,1],
-        'wspace': 0.2,'hspace': 0.4})
+fig,ax=plt.subplots(num,
+                    2, 
+                    figsize=(4,3), 
+                    gridspec_kw={'width_ratios': [2,1], 'wspace': 0.2, 'hspace': 0.4}
+)
 
 ax[0][1].set_title('Shape',size=15)
 ax[0][0].set_title('Light Curve (Train Dataset)',size=15)
 ax[num-1][0].set_xlabel('Phase',size=13)
 ph = np.linspace(-1,1,len(processed_train_lc[0]))
-# advance = 60
 
 i = 0
-for i in np.arange(0,num):
+for i in np.arange(0, num):
     k = np.random.randint(0, len(processed_train_lc)-1)
-    ax[i][1].tick_params(left = False, right = False , labelleft = False ,labelbottom = False, bottom = False)
+    ax[i][1].tick_params(left = False, right = False , labelleft = False, labelbottom = False, bottom = False)
     if(i<num-1): ax[i][0].tick_params(labelbottom = False, bottom = False)
-    img = ax[i][1].imshow(train_shape[k],cmap='inferno')
+    img = ax[i][1].imshow(train_shape[k], cmap='inferno')
     plt.colorbar(img)
-    ax[i][0].set_ylabel('Flux',size=13)
-    ax[i][0].set_ylim(-0.5,1.5)
-    # ax[i][0].scatter(ph, vald_lc_scaled_append[k],color = 'black',marker='.')
-    ax[i][0].plot(ph, processed_train_lc[k],color = 'tab:red',linewidth='2')
+    ax[i][0].set_ylabel('Flux', size=13)
+    ax[i][0].set_ylim(-0.5, 1.5)
+    ax[i][0].plot(ph, processed_train_lc[k], color = 'tab:red', linewidth='2')
     ax[i][0].grid('on')
     i = i + 1
-plt.savefig('/scratch/abraham/Documents/mega_git/mega/ml_model/april9_2024_model/plot_train_lc.png')
+plt.savefig('plot_train_lc.png')
 plt.close()
 
 # Plot - Vald LCs
@@ -233,20 +266,21 @@ for i in np.arange(0,num):
     ax[i][0].plot(ph, processed_vald_lc[k],color = 'tab:red',linewidth='2')
     ax[i][0].grid('on')
     i = i + 1
-plt.savefig('/scratch/abraham/Documents/mega_git/mega/ml_model/april9_2024_model/plot_vald_lc.png')
+plt.savefig(base_model_folder+'plot_vald_lc.png')
 plt.close()
 
 # Model path to be saved to
-model_save_path = "/scratch/abraham/Documents/mega_git/mega/ml_model/april9_2024_model/april9_2024_model_unfDist_LDC_size_horz_scale.h5"
+model_save_path = base_model_folder+"april24_2024_model_unfDist_LDC_size_horz_scale.h5"
 print(f"model_save_path = {model_save_path}")
 
-no_epochs = int(200) # For testing start with small value of 3 or 5
+no_epochs = int(3) # For testing start with small value of 3 or 5
 print("no_epochs =",no_epochs)
 
 user_input = input("Do you want to run the code? (y/n): ")
 if user_input.lower() != "y":
     sys.exit("EXIT: User declined to continue the program")
 
+tic = time.perf_counter()
 print("Creating ML Pipeline")
 # 6. ML Pipeline
 ## Train Set
@@ -266,58 +300,136 @@ print(vald_dataset)
 
 # CNN Model
 input_shape = np.array(np.shape(processed_train_lc[0]))
-print("np.shape(input_shape) = ",input_shape[0])
+print("np.shape(input_shape) =", input_shape[0])
 
 output_shape = np.array(np.shape(train_shape[0]))
-print("np.shape(input_shape) = ",output_shape[0],output_shape[1])
+print("np.shape(input_shape) =", output_shape[0], output_shape[1])
 
-START = input_shape[0] # 94 #
-END = output_shape[0] # 38  # pixel size along one axis
-print("Start = ",START)
-print("End = ",END)
+START = input_shape[0]
+END = output_shape[0]
+print("Start =", START)
+print("End =", END)
 
-conv_ip = keras.layers.Input(shape=(START,),name='Input')
-x= keras.layers.Reshape((START, 1), input_shape=(START,),name='reshape_1')(conv_ip)
+conv_ip = keras.layers.Input(shape=(START,), name='Input')
+x= keras.layers.Reshape((START, 1), input_shape=(START,), name='reshape_1')(conv_ip)
 x= keras.layers.BatchNormalization()(x)
 
-x=keras.layers.Conv1D(16,kernel_size=5,strides=1,activation='relu',name='conv16_5', padding='same')(x)
-x=keras.layers.Conv1D(16,kernel_size=5,strides=1,activation='relu',name='second_conv16_5', padding='same')(x)
-x=keras.layers.MaxPool1D(5,strides=2,data_format='channels_last',name='maxpool_1', padding='same')(x) #400
+x=keras.layers.Conv1D(16,
+                      kernel_size=5,
+                      strides=1,
+                      activation='relu',
+                      name='conv16_5', 
+                      padding='same')(x)
 
-x=keras.layers.Conv1D(32,kernel_size=5,strides=1,activation='relu',name='first_conv32_5', padding='same')(x)
-x=keras.layers.Conv1D(32,kernel_size=5,strides=1,activation='relu',name='second_conv32_5', padding='same')(x)
-x=keras.layers.MaxPool1D(5,strides=2,data_format='channels_last',name='maxpool_2', padding='same')(x) #200
+x=keras.layers.Conv1D(16,
+                      kernel_size=5,
+                      strides=1,
+                      activation='relu',
+                      name='second_conv16_5', 
+                      padding='same')(x)
 
-x=keras.layers.Conv1D(64,kernel_size=5,strides=1,activation='relu',name='first_conv64_5', padding='same')(x)
-x=keras.layers.Conv1D(64,kernel_size=5,strides=1,activation='relu',name='second_conv64_5', padding='same')(x)
-x=keras.layers.MaxPool1D(5,strides=2,data_format='channels_last',name='maxpool_3', padding='same')(x) #100
+x=keras.layers.MaxPool1D(5,
+                         strides=2,
+                         data_format='channels_last',
+                         name='maxpool_1', 
+                         padding='same')(x)
+
+x=keras.layers.Conv1D(32,
+                      kernel_size=5,
+                      strides=1,
+                      activation='relu',
+                      name='first_conv32_5', 
+                      padding='same')(x)
+
+x=keras.layers.Conv1D(32,
+                      kernel_size=5,
+                      strides=1,
+                      activation='relu',
+                      name='second_conv32_5', 
+                      padding='same')(x)
+
+x=keras.layers.MaxPool1D(5,
+                         strides=2,
+                         data_format='channels_last',
+                         name='maxpool_2', 
+                         padding='same')(x) #200
+
+x=keras.layers.Conv1D(64,
+                      kernel_size=5,
+                      strides=1,
+                      activation='relu',
+                      name='first_conv64_5', 
+                      padding='same')(x)
+
+x=keras.layers.Conv1D(64,
+                      kernel_size=5,
+                      strides=1,
+                      activation='relu',
+                      name='second_conv64_5', 
+                      padding='same')(x)
+
+x=keras.layers.MaxPool1D(5,
+                         strides=2,
+                         data_format='channels_last',
+                         name='maxpool_3', 
+                         padding='same')(x) #100
 
 x=keras.layers.Flatten(name='flat_1')(x)
 
-x2=keras.layers.Dense(256,name='dense_layer_5',activation='relu')(x)
-x2=keras.layers.Dense(256,name='dense_layer_6',activation='relu')(x2)
+x2=keras.layers.Dense(256, name='dense_layer_5', activation='relu')(x)
+x2=keras.layers.Dense(256, name='dense_layer_6', activation='relu')(x2)
 
-x2= keras.layers.Dense(END**2,name='dense_layer_u',activation='relu')(x2)
-x2 = keras.layers.Reshape(target_shape=(END, END, 1),name='reshape_2')(x2)
+x2= keras.layers.Dense(END**2, name='dense_layer_u', activation='relu')(x2)
+x2 = keras.layers.Reshape(target_shape=(END, END, 1), name='reshape_2')(x2)
 
-x2=keras.layers.Conv2D(32,kernel_size=(3,3),strides=1,activation='relu',name='second_conv64_52', padding='same')(x2)
-x2=keras.layers.Conv2D(32,kernel_size=(3,3),strides=1,activation='relu',name='second_conv64_522', padding='same')(x2)
-x2=keras.layers.Conv2D(16,kernel_size=(3,3),strides=1,activation='relu',name='second_conv64_524', padding='same')(x2)
-x2=keras.layers.Conv2D(1,kernel_size=3,strides=1,activation='relu',name='second_conv64_53', padding='same')(x2)
-conv_op = keras.layers.Reshape(target_shape=(END, END),name='reshape_3')(x2)
+x2=keras.layers.Conv2D(32, 
+                       kernel_size=(3,3), 
+                       strides=1,
+                       activation='relu',
+                       name='second_conv64_52', 
+                       padding='same')(x2)
+
+x2=keras.layers.Conv2D(32, 
+                       kernel_size=(3,3), 
+                       strides=1,
+                       activation='relu',
+                       name='second_conv64_522', 
+                       padding='same')(x2)
+
+x2=keras.layers.Conv2D(16,
+                       kernel_size=(3,3),
+                       strides=1,
+                       activation='relu',
+                       name='second_conv64_524', 
+                       padding='same')(x2)
+
+x2=keras.layers.Conv2D(1,
+                       kernel_size=3,
+                       strides=1,
+                       activation='relu',
+                       name='second_conv64_53', 
+                       padding='same')(x2)
+
+conv_op = keras.layers.Reshape(target_shape=(END, END), name='reshape_3')(x2)
+
 model = keras.Model(inputs=conv_ip, outputs=conv_op, name="predict_shape_from_LC")
 model.summary()
+
 print("Model is defined")
 
 # Compile the model
 model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.0001), loss='mse')
 print("Model is compiled")
 
-#Patience early stopping
-es = keras.callbacks.EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=30)
+# Patience early stopping
+es = keras.callbacks.EarlyStopping(monitor='val_loss', 
+                                   mode='min', 
+                                   verbose=1, 
+                                   patience=30
+)
 print("Early stopping defined")
 
-#Learning rate scheduler
+# Learning rate scheduler
 def step_decay(epoch):
 	initial_lrate = 0.001
 	drop = 0.5
@@ -327,15 +439,43 @@ def step_decay(epoch):
 lr_sched = keras.callbacks.LearningRateScheduler(step_decay)
 print("Learning rate scheduler defined")
 
+# Model checkpoint
+checkpoint_path = base_model_folder+"april24_2024_model/ckpt/checkpoint.weights.h5"
+
+model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+    checkpoint_path,
+    monitor='val_loss',
+    verbose=0,
+    save_best_only=False,
+    save_weights_only=True,
+    mode='min',
+    save_freq='epoch',
+    initial_value_threshold=None
+)
+print("Model checkpoint defined")
+
 # Training 
 print("Training will start now")
 
-history = model.fit(train_dataset, epochs=no_epochs,verbose=2, validation_data=vald_dataset,callbacks=[es,lr_sched])
+history = model.fit(train_dataset, 
+                    epochs=no_epochs,
+                    verbose=2, 
+                    validation_data=vald_dataset,
+                    callbacks=[es, lr_sched, model_checkpoint_callback]
+)
 
 # Save Model
-save_model(model,str(model_save_path))
+save_model(model, str(model_save_path))
 print("Model saved")
 
+plt.plot(history.history['loss'], label="Training Loss")
+plt.plot(history.history['val_loss'], label="Validation Loss")
+plt.legend()
+plt.savefig(base_model_folder+'history_loss_graph.png')
+plt.close()
+
+toc = time.perf_counter()
+print(toc-tic, " s")
 
 tf.keras.backend.clear_session()
 print("End of code")
